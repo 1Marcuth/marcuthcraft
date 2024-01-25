8c3c222b87cbeee421dbc608572f440943e4428c
TantoFaz <thiago.playgames15@gmail.com>
2024-01-25 20:30:33 -0300

    ...

--- src/utils/wait.ts
+++ src/utils/wait.ts
@@ -1,5 +1,5 @@
-function wait(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+function wait(time: number) {
+    return new Promise(resolve => setTimeout(resolve, time))
 }
 
 export default wait
