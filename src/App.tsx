007e84ee0495c807e0c8b075604701616e465de8
TantoFaz <thiago.playgames15@gmail.com>
2024-01-25 20:12:12 -0300

    update node version

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: FC = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<HomePage/>}/>
+                <Route path="/*" element={<HomePage/>}/>
             </Routes>
         </Router>
     )
