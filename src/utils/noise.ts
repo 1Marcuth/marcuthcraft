9e3508dfa922fda00c6b6ab40479c44c9987b385
TantoFaz <thiago.playgames15@gmail.com>
2024-01-27 19:25:07 -0300

    d

--- src/utils/noise.ts
+++ src/utils/noise.ts
@@ -22,70 +22,70 @@ export type CaveGeneratorProps = {
 type CaveMap = number[][]
 
 class CaveGenerator {
-    public props: CaveGeneratorProps;
-    private prng: PRNG;
+    public props: CaveGeneratorProps
+    private prng: PRNG
 
     private constructor(props: CaveGeneratorProps) {
         this.props = props;
-        this.prng = new PRNG(this.props.seed);
+        this.prng = new PRNG(this.props.seed)
     }
 
     public static generate(props: CaveGeneratorProps): CaveMap {
-        const generator = new CaveGenerator(props);
-        const { iterations } = generator.props;
+        const generator = new CaveGenerator(props)
+        const { iterations } = generator.props
 
-        const caveMap = generator.generateInitialMap();
+        const caveMap = generator.generateInitialMap()
 
         for (let i = 0; i < iterations; i++) {
-            generator.iterateMap(caveMap);
+            generator.iterateMap(caveMap)
         }
 
-        return caveMap;
+        return caveMap
     }
 
     private generateInitialMap(): CaveMap {
-        const { width, height, density } = this.props;
+        const { width, height, density } = this.props
         const caveMap: CaveMap = [];
 
         for (let x = 0; x < width; x++) {
             caveMap[x] = [];
             for (let y = 0; y < height; y++) {
-                let xyz = this.prng.next();
-                caveMap[x][y] = xyz < density ? 0 : 1;
+                let xyz = this.prng.next()
+                caveMap[x][y] = xyz < density ? 0 : 1
             }
         }
 
-        return caveMap;
+        return caveMap
     }
 
     private iterateMap(caveMap: CaveMap): void {
-        const { width, height } = this.props;
+        const { width, height } = this.props
 
         for (let x = 0; x < width; x++) {
             for (let y = 0; y < height; y++) {
-                const wallCount = this.getNeighborsWalls(caveMap, x, y);
-                caveMap[x][y] = wallCount > 4 ? 1 : 0;
+                const wallCount = this.getNeighborsWalls(caveMap, x, y)
+                caveMap[x][y] = wallCount > 4 ? 1 : 0
             }
         }
     }
 
     private getNeighborsWalls(cavesMap: CaveMap, x: number, y: number) {
-        const { width, height } = this.props;
-        let wallCount = 0;
+        const { width, height } = this.props
+        let wallCount = 0
 
         for (let i = -1; i < 2; i++) {
             for (let j = -1; j < 2; j++) {
                 if (i !== x || j !== y) {
                     if (x + i < 0 || y + j < 0 || x + i >= width || y + j >= height) {
-                        wallCount++;
+                        wallCount++
                     } else {
-                        wallCount += cavesMap[x + i][y + j] ? 1 : 0;
+                        wallCount += cavesMap[x + i][y + j] ? 1 : 0
                     }
                 }
             }
         }
 
-        return wallCount;
+        return wallCount
     }
 }
 class Noise {
@@ -121,7 +121,6 @@ class Noise {
     }
 
     public static generateCavesMap(props: CaveGeneratorProps) {
-        console.log(CaveGenerator.generate(props).length)
         return CaveGenerator.generate(props)
     }
 }
